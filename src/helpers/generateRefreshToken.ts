ffb39c903482e5e97ea4fad994f4620184d6b3b3
igorsilvaj <igorjsilvabiz@gmail.com>
2023-06-22 01:30:29 -0300

    :sparkles:feat: expiration can come from env

--- src/helpers/generateRefreshToken.ts
+++ src/helpers/generateRefreshToken.ts
@@ -1,9 +1,11 @@
-import dayjs from "dayjs";
+import dayjs, { ManipulateType } from "dayjs";
 import { client } from "../prisma/client";
 
 export class GenerateRefreshToken {
   async execute(userId: string) {
-    const expiresIn = dayjs().add(30, 'days').unix()
+    const exp = process.env.REFRESH_TOKEN_EXP.split(',')
+    
+    const expiresIn = dayjs().add(+exp[0] || 30, exp[1] as ManipulateType || 'days').unix()
 
     const generateRefreshToken = await client.refreshToken.create({
       data: {
--- src/helpers/generateToken.ts
+++ src/helpers/generateToken.ts
@@ -2,7 +2,9 @@ import { sign } from "jsonwebtoken";
 
 export class GenerateToken {
   async execute(userId: string) {
-    const token = sign({ subject: userId }, process.env.JWT_TOKEN, { expiresIn: "15m" })
+    const exp = process.env.SESSION_TOKEN_EXP
+    
+    const token = sign({ subject: userId }, process.env.JWT_TOKEN, { expiresIn: exp || "15m" })
     return token
   }
 }
